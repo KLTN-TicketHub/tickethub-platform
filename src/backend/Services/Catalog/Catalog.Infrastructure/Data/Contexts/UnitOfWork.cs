959442f2cfab76994687a34f5b02ef953523f8df
hngoxuantg <hngoxuantg.dev@gmail.com>
2026-05-17 15:01:40 +0700

    Refactor UnitOfWork to use CatalogDbContext
    Updated UnitOfWork to inherit from BaseUnitOfWork<CatalogDbContext> and accept CatalogDbContext in its constructor. Removed unused using directives. Modified IUnitOfWork interface to inherit from IBaseUnitOfWork without specifying a DbContext type.

--- src/backend/Services/Catalog/Catalog.Infrastructure/Data/Contexts/UnitOfWork.cs
+++ src/backend/Services/Catalog/Catalog.Infrastructure/Data/Contexts/UnitOfWork.cs
@@ -1,15 +1,11 @@
 ﻿using BuildingBlocks.Infrastructure.Data;
 using Catalog.Domain.Interfaces;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Catalog.Infrastructure.Data.Contexts
 {
-    public class UnitOfWork : BaseUnitOfWork<IdentityDbContext>, IUnitOfWork
+    public class UnitOfWork : BaseUnitOfWork<CatalogDbContext>, IUnitOfWork
     {
-        public UnitOfWork(IdentityDbContext dbContext) : base(dbContext)
+        public UnitOfWork(CatalogDbContext dbContext) : base(dbContext)
         {
         }
     }
