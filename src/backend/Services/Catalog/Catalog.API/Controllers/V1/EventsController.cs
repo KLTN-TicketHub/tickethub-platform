16c0374336acc1c902786a4e40c49261b82084fd
hngoxuantg <hngoxuantg.dev@gmail.com>
2026-06-13 15:26:05 +0700

    refactor: remove unused using directives from CreateEvent command files

--- src/backend/Services/Catalog/Catalog.API/Controllers/V1/EventsController.cs
+++ src/backend/Services/Catalog/Catalog.API/Controllers/V1/EventsController.cs
@@ -1,7 +1,6 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Catalog.API.Controllers.V1
--- src/backend/Services/Catalog/Catalog.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
+++ src/backend/Services/Catalog/Catalog.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Catalog.Application.Features.Events.Commands.CreateEvent
+﻿namespace Catalog.Application.Features.Events.Commands.CreateEvent
 {
     public class CreateEventCommandHandler
     {
