1a4a7b6594b48d0e2ff3ccc96a7643acd1aa1cc6
hngoxuantg <hngoxuantg.dev@gmail.com>
2026-07-28 06:35:18 +0700

    fix: downgrade RabbitMQ health check package version to 8.0.2 and update health check implementation across services

--- src/backend/Services/Catalog/Catalog.API/Extensions/HealthCheckExtension.cs
+++ src/backend/Services/Catalog/Catalog.API/Extensions/HealthCheckExtension.cs
@@ -1,27 +1,15 @@
-using RabbitMQ.Client;
-
 namespace Catalog.API.Extensions
 {
     public static class HealthCheckExtension
     {
         public static IServiceCollection AddCustomHealthChecks(this IServiceCollection services, IConfiguration configuration)
         {
+            string rabbitMqUri = $"amqp://{configuration["RabbitMq:Username"]}:{configuration["RabbitMq:Password"]}@{configuration["RabbitMq:Host"]}:{configuration["RabbitMq:Port"]}/";
+
             services.AddHealthChecks()
                 .AddSqlServer(configuration.GetConnectionString("PrimaryDbConnection")!, name: "sqlserver")
                 .AddRedis(configuration["Redis:ConnectionString"]!, name: "redis")
-                .AddRabbitMQ(sp =>
-                {
-                    ConnectionFactory factory = new ConnectionFactory
-                    {
-                        HostName = configuration["RabbitMq:Host"],
-                        Port = int.Parse(configuration["RabbitMq:Port"]!),
-                        UserName = configuration["RabbitMq:Username"],
-                        Password = configuration["RabbitMq:Password"],
-                        VirtualHost = configuration["RabbitMq:VirtualHost"]
-                    };
-
-                    return factory.CreateConnectionAsync();
-                }, name: "rabbitmq");
+                .AddRabbitMQ(rabbitMqUri, name: "rabbitmq");
 
             return services;
         }
